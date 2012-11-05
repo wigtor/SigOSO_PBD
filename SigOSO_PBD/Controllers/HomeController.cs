b2aba6cd2c131806f6ac325d4d9b4db922b26af7
wigtor <victor.floress@usach.cl>
2012-11-05 00:12:53 -0400

    Realizada primera conexión a postgreSQL, se puede probar en la vista about.aspx

--- SigOSO_PBD/Controllers/HomeController.cs
+++ SigOSO_PBD/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Npgsql;
 
 namespace SigOSO_PBD.Controllers
 {
@@ -17,7 +18,38 @@ namespace SigOSO_PBD.Controllers
 
         public ActionResult About()
         {
+
+            ViewBag.primerValor = cargaUnidades();
             return View();
+        }
+
+        public string cargaUnidades()
+        {
+            string resultado = "";
+            NpgsqlConnection con = new NpgsqlConnection("Server=localhost;Port=5432;UserId=SigOSO_user;Password=pbd2012;Database=SigOSO");
+            try
+            {
+                con.Open();
+
+            }
+            catch (Exception)
+            {
+                return "ERROR EN CONEXIÓN A POSTGRE";
+            }
+            //Un insert o un update o un delete
+            /*
+            NpgsqlCommand comando = new NpgsqlCommand("INSERT INTO unidad_material (nombre_unidad, abreviatura_unidad) VALUES ('kilogramos', 'kg')", con);
+            int resultado = comando.ExecuteNonQuery();
+            */
+
+            //Un select
+            NpgsqlCommand comando = new NpgsqlCommand("SELECT * FROM unidad_material", con);
+            NpgsqlDataReader resultQuery =  comando.ExecuteReader();
+            if (resultQuery.Read()) {       
+                resultado = resultQuery.GetString(1);
+            }
+            return resultado;
+
         }
     }
 }
--- SigOSO_PBD/Views/Home/About.aspx
+++ SigOSO_PBD/Views/Home/About.aspx
@@ -9,4 +9,7 @@
     <p>
         Este corresponde a la entrega final del trabajo de Proyecto de base de datos 2-2012. El proyecto ha sido llamado SigOSO
     </p>
+    <p>
+        Haciendo una prueba de un select: "SELECT * FROM unidad_material" y se ha obtenido el primer valor resultante: <%: ViewBag.primerValor %>
+    </p>
 </asp:Content>
